c8d6375f5f87a3a83cf45678844654c9f3afa82d
yanvow <yanis.seddik@gmail.com>
2022-04-03 15:10:34 +0200

    week4

--- Assets/Scripts/Core/Behaviors/GhostSheepBehavior.cs
+++ Assets/Scripts/Core/Behaviors/GhostSheepBehavior.cs
@@ -3,11 +3,17 @@ using UnityEngine;
 
 public class GhostSheepBehavior : AgentBehaviour
 {    
-    public void Start(){
+    public AudioSource sheepAudio;
+    public AudioSource ghostAudio; 
+    
+    void Start()
+    {
+    
     }
 
     void Update()
     {
+        changeMode();
         tag = GetComponent<CelluloAgent>().tag;
         if(tag == "CelluloGhost"){
             GetComponent<CelluloAgent>().SetVisualEffect(0, Color.yellow, 0);
@@ -26,20 +32,43 @@ public class GhostSheepBehavior : AgentBehaviour
         Vector3 position = transform.position;
         GameObject closest = null;
         float distance = Mathf.Infinity ;
+        
         foreach (GameObject CelluloDog in CelluloDogs){
             Vector3 diff = CelluloDog.transform.position - position;
             float curDistance = diff.sqrMagnitude;
-            if (curDistance < distance && curDistance < 20){    
-                closest = CelluloDog;
-                distance = curDistance;
-            }
+            if (curDistance < distance){
+                if ((tag == "CelluloSheep" && curDistance < 20) || tag == "CelluloGhost"){    
+                    closest = CelluloDog;
+                    distance = curDistance;
+                }
+            } 
         }
         steering.linear = new  Vector3 (0,0,0);
+
         if (distance != Mathf.Infinity){
-            steering.linear = -(closest.transform.position - position) * agent.maxAccel;
+            if(tag == "CelluloSheep"){
+                steering.linear = -(closest.transform.position - position) * agent.maxAccel;
+            }else {
+                steering.linear = (closest.transform.position - position) * (agent.maxAccel-10);
+            }
         }
         steering.linear = this.transform.parent.TransformDirection(Vector3.ClampMagnitude(steering.linear , agent.maxAccel));
 
        return steering;
+    }
+    public void changeMode(){
+        float seconds = Mathf.FloorToInt(Time.timeSinceLevelLoad );
+        if (seconds % 56 < 28){
+            GetComponent<CelluloAgent>().tag = "CelluloSheep";
+        }else{
+            GetComponent<CelluloAgent>().tag = "CelluloGhost";
+        }
+        if(Time.timeScale != 0){
+            if(seconds % 56 == 0){
+                sheepAudio.Play();
+            }else if(seconds % 56 == 27){
+                ghostAudio.Play();
+            }
+        } 
     }
 }
