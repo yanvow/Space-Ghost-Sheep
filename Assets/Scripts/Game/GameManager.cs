15668649e017f53a9f8417e4ffefc22663d8c118
yanvow <yanis.seddik@gmail.com>
2022-04-21 15:49:00 +0200

    milestone 2

--- Assets/Scripts/Game/GameManager.cs
+++ Assets/Scripts/Game/GameManager.cs
@@ -3,23 +3,17 @@ using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
-    public Button startButton;
 
    private void Awake(){
-       Time.timeScale = 0f;
    }
 
    private void OnEnable(){
-      startButton.onClick.AddListener(StartGame);
 
    }
 
    private void OnDisable(){
-      startButton.onClick.RemoveListener(StartGame);
    }
 
    private void StartGame(){
-      Time.timeScale = 1f;
-      startButton.gameObject.SetActive(false);
    }
 }
